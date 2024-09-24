532f63f3206bb3d86e840c90a690b5bff4b0cfc7
MarkY <markyontel2000@gmail.com>
2024-09-24 22:15:11 +0300

    Update ViewHelper.js

--- threejs/ViewHelper.js
+++ threejs/ViewHelper.js
@@ -12,7 +12,6 @@ function ViewHelper(editorCamera, container) {
 	var scope = this;
 
 	panel.dom.addEventListener('mouseup', function (event) {
-		console.log('idan');
 		event.stopPropagation();
 
 		scope.handleClick(event);
